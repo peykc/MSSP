9d39f4eb617f0094926d35849a9727e09817400a
Peytiepie05 <peytonkossex@gmail.com>
2026-07-20 05:10:23 -0400

    native paytch guide app open touch device

--- mssp_app/public/service-worker.js
+++ mssp_app/public/service-worker.js
@@ -1,4 +1,4 @@
-const RELEASE_ID = "v360";
+const RELEASE_ID = "v361";
 const CACHE_PREFIX = "mssp-";
 const SHELL_CACHE_PREFIX = `${CACHE_PREFIX}shell-`;
 const DATA_CACHE_PREFIX = `${CACHE_PREFIX}data-`;
